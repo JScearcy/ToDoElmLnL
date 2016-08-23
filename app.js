eae4ecf7aa91198931c71286f50bc926a479da63
JScearcy <jacobscearcy@gmail.com>
2016-08-23 14:34:50 -0500

    js step 2 - add todo container and listeners for buttons, create todo functionality

--- app.js
+++ app.js
@@ -4,11 +4,9 @@
         todoInput = document.getElementById('todoName'),
 		todoTemplate = document.getElementById('todoTemplate').innerHTML,
 		todoContainer = document.getElementById('todos'),
-		toggleCompleted = document.getElementById('toggleCompletedTodos'),
-		completedVisible = true;
+		toggleCompleted = document.getElementById('toggleCompletedTodos');
 
     todoInput.focus();
-    todoContainer.addEventListener('click', CheckBoxHandler);
 	toggleCompleted.addEventListener('click', ToggleCompletedTodos);
     document.getElementById('addTodo').addEventListener('click', ToDoCreate);
     document.getElementById('clearCompletedTodos').addEventListener('click', ClearCompletedTodos);
@@ -37,54 +35,11 @@
 
     function ClearCompletedTodos(e) {
         e.preventDefault();
-
-        MapCompletedNodes(RemoveTodo);
+        throw Error('Not Implemented');
     }
 
     function ToggleCompletedTodos(e) {
         e.preventDefault();
-
-        VisibilityHandler();
-        MapCompletedNodes(ToggleTodo);
-    }
-
-    function VisibilityHandler() {
-        completedVisible = !completedVisible;
-
-        if (completedVisible) {
-            toggleCompleted.innerHTML = "Hide Completed"
-        } else if (!completedVisible) {
-            toggleCompleted.innerHTML = "Show Completed";
-        }
-    }
-
-    function MapCompletedNodes(mapFn) {
-        Array.from(document.getElementsByClassName('todoItem'))
-            .filter(function(el) {
-                return el.querySelector('.completeCheck').checked;
-            }).map(function(el) {
-                mapFn(el);
-            });
-    }
-
-    function RemoveTodo(el) {
-        el.parentNode.removeChild(el);
-    }
-
-    function ToggleTodo(el) {
-        if (!el.classList.contains('hidden')) {
-            el.classList.add('hidden');
-        } else {
-            el.classList.remove('hidden');
-        }
-    }
-
-    function CheckBoxHandler(e) {
-        var target = e.target,
-            parent = e.target.parentNode.parentNode;
-        
-        if (!completedVisible && target.classList.contains('completeCheck') && target.checked) {
-            parent.classList.add('hidden');
-        }
+        throw Error('Not Implemented');
     }
 })(window, document);
